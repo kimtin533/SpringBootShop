a328ca170baa06e9f5194703f122b88c6b04c4d8
kimtin533 <tinhoangkim533@gmail.com>
2024-06-03 21:54:05 +0700

    add order

---
+++ src/main/java/com/javaweb/springboot/models/Order.java
@@ -0,0 +1,68 @@
+package com.javaweb.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "fullname", length = 100)
+    private String fullName;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "phone_number",nullable = false, length = 100)
+    private String phoneNumber;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @Column(name = "note", length = 100)
+    private String note;
+
+    @Column(name="order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_money")
+    private Float totalMoney;
+
+    @Column(name = "shipping_method")
+    private String shippingMethod = "";
+
+    @Column(name = "shipping_address")
+    private String shippingAddress = "";
+
+    @Column(name = "shipping_date")
+    private LocalDate shippingDate;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod = "";
+
+    @Column(name = "active")
+    private Boolean active;//thuộc về admin
+}
